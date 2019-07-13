58bb32eebc1e32ad966c1a0166ccadaae174edc3
dandenney <dd.subscriptions@gmail.com>
2019-07-13 19:40:52 -0400

    feat(case): change status color based on status

---
+++ src/helpers/statusColor.js
@@ -0,0 +1,15 @@
+const statusColor = status => {
+  console.log(status)
+  switch (status) {
+    case "Open":
+      return "open"
+    case "Pending Trial":
+      return "pending"
+    case "Solved":
+      return "solved"
+    default:
+      return null
+  }
+}
+
+export default statusColor
