9c0518a9d2504ae196e157456acca67908f2a6a0
dandenney <dd.subscriptions@gmail.com>
2019-07-21 23:25:21 -0400

    refactor(share): tweak image sizes and captions

--- src/components/share.js
+++ src/components/share.js
@@ -25,46 +25,82 @@ const Share = ({
       <Flex sx={{ justifyContent: "center", flexWrap: "wrap" }}>
         <Box
           as="figure"
-          sx={{ m: 0, p: 8, width: ["100%", null, null, null, "50%"] }}
+          sx={{ m: 0, p: 8, width: ["100%", null, null, "50%", null, "25%"] }}
         >
           <Img
             alt="a"
             fluid={instagramImage.localFiles[0].childImageSharp.fluid}
             width={540}
           />
-          <figcaption>Sized for the Instagram Square</figcaption>
+          <figcaption
+            sx={{
+              color: "mute",
+              fontSize: "75%",
+              mt: 8,
+              textAlign: "center",
+            }}
+          >
+            (Sized for the Instagram Square)
+          </figcaption>
         </Box>
         <Box
           as="figure"
-          sx={{ m: 0, p: 8, width: ["100%", null, null, null, "50%"] }}
+          sx={{ m: 0, p: 8, width: ["100%", null, null, "50%", null, "25%"] }}
         >
           <Img
             alt="a"
             fluid={instagramStoryImage.localFiles[0].childImageSharp.fluid}
             width={540}
           />
-          <figcaption>Sized for the Instagram Story</figcaption>
+          <figcaption
+            sx={{
+              color: "mute",
+              fontSize: "75%",
+              mt: 8,
+              textAlign: "center",
+            }}
+          >
+            (Sized for the Instagram Story)
+          </figcaption>
         </Box>
         <Box
           as="figure"
-          sx={{ m: 0, p: 8, width: ["100%", null, null, null, "50%"] }}
+          sx={{ m: 0, p: 8, width: ["100%", null, null, "50%", null, "25%"] }}
         >
           <Img
             alt="a"
             fluid={facebookImage.localFiles[0].childImageSharp.fluid}
           />
-          <figcaption>Sized for the Facebook Timeline</figcaption>
+          <figcaption
+            sx={{
+              color: "mute",
+              fontSize: "75%",
+              mt: 8,
+              textAlign: "center",
+            }}
+          >
+            (Sized for the Facebook Timeline)
+          </figcaption>
         </Box>
         <Box
           as="figure"
-          sx={{ m: 0, p: 8, width: ["100%", null, null, null, "50%"] }}
+          sx={{ m: 0, p: 8, width: ["100%", null, null, "50%", null, "25%"] }}
         >
           <Img
             alt="a"
             fluid={twitterImage.localFiles[0].childImageSharp.fluid}
             width={440}
           />
-          <figcaption>Sized for the Twitter Timeline</figcaption>
+          <figcaption
+            sx={{
+              color: "mute",
+              fontSize: "75%",
+              mt: 8,
+              textAlign: "center",
+            }}
+          >
+            (Sized for the Twitter Timeline)
+          </figcaption>
         </Box>
       </Flex>
     </section>
