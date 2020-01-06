0bedab5891fcf3c26eb2517ca094707374de1876
Dan Denney <dandenney@Dans-MacBook-Pro.local>
2020-01-05 20:05:35 -0500

    feat(global): add a favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -121,6 +121,24 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "True Crime Researchers",
+        short_name: "TCR",
+        start_url: "/",
+        background_color: "#fcfcfc",
+        theme_color: "#4f5e78",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
